98b70f69043f07d20276d4c1bd782a1e865ec7c0
ZhdanovichTimofey <71005308+ZhdanovichTimofey@users.noreply.github.com>
2020-12-11 14:53:54 +0300

    исправил оформление в соответствии с PEP8 в модуле model

--- model.py
+++ model.py
@@ -7,14 +7,15 @@ AttributeError - сделать exception
 
 '''
 
-#model.py
+# model.py
 
 import numpy as np
 import re
 import codecs
        
-class Node ():
-    '''
+
+class Node:
+    """
     Класс, реализующий вершину двуцветного графа созвездий и ставящий 
     каждому созвездию в соответсвие его имена на русском и латинском а также
     его соседей.
@@ -23,23 +24,23 @@ class Node ():
     эта вершина;
     _full_file_str - внутренняя переменная, получаемая при вызове из
     класса Graph, которая содержит информацию о звездном небе в виде строки.
-    '''
-    def __init__ (self, stell_name, _full_file_str):
+    """
+    def __init__(self, stell_name, _full_file_str):
         self.first_name = stell_name
         self.mark = 0
         self.names, self.neighbours = \
             self._read_neighbours(_full_file_str, self.first_name, )
     
     def _read_neighbours(self, full_file_str, name):
-        '''
-        Приватный метод, находящий для данного созвездия всех его соседей и 
+        """
+        Приватный метод, находящий для данного созвездия всех его соседей и
         все его имена с помощью поиска в строке full_file_str. Вызывается
         при инициализации.
         -----------------------------------------------------------------------
         name - трёхбуквенное имя созвездия, для которого производится поиск;
-        full_file_str - переменная, которая содержит информацию о 
+        full_file_str - переменная, которая содержит информацию о
         звездном небе в виде строки.
-        '''
+        """
         find_regex = self._get_regex(name + '(-\w+)+:(-\w+)+\.')
         namesList_regex = self._get_regex(name + '(-\w+)+' + '\:')
         neighboursList_regex = self._get_regex('\:' + '(-\w+)+' + '\.')
@@ -60,8 +61,8 @@ class Node ():
         for i in range(len(nameParts)):
             nameParts[i] = nameParts[i][1:]
         
-        names = np.array([name], dtype = '<U13')
-        neighbours = np.array(neighbours, dtype = '<U13')
+        names = np.array([name], dtype='<U13')
+        neighbours = np.array(neighbours, dtype='<U13')
         
         if len(nameParts) == 2:
             names = np.append(names, nameParts)
@@ -75,67 +76,66 @@ class Node ():
             part_RU = nameParts[2] + ' ' + nameParts[3]
             nameParts = [part_LAT, part_RU]
             names = np.append(names, nameParts)
-        return (names, neighbours)
+        return names, neighbours
 
-    def _get_regex(self, template:str):
-        '''
+    def _get_regex(self, template: str):
+        """
         Приватный метод, возвращает регулярное выражение по строке.
         -----------------------------------------------------------------------
         template - строка-шаблон для создания регулярного выражения.
-        '''
+        """
         return re.compile(template)
         
-    def _find_str (self, regex, object_str):
-        '''
-        Приватный метод, возвращает один, первый, результат поиска в строке 
+    def _find_str(self, regex, object_str):
+        """
+        Приватный метод, возвращает один, первый, результат поиска в строке
         по регулярному выражению.
         -----------------------------------------------------------------------
         regex - регулярное выражение, по которому происходит поиск;
         object_str - строка, в которой происходит поиск.
-        '''
+        """
         _match = regex.search(object_str)
         return _match.group()
 
 
-
-class Graph ():
-    '''
+class Graph:
+    """
     Класс, реализующий двуцветный граф созвездий в виде словаря "трехбуквенное
     обозначение созвездия : объект Node этого созвездия".
     ---------------------------------------------------------------------------
     file_name - имя файла, в котором содержится информация о звёздном небе.
-    '''
-    def __init__(self, file_name:str):
+    """
+    def __init__(self, file_name: str):
         self.file_name = file_name
         self.constellations = self._read_constellations(file_name)
 
-    def _read_constellations (self, file_name):
-        '''
-        Приватный метод, читающий файл и возвращающий словарь созвездий. 
+    def _read_constellations(self, file_name):
+        """
+        Приватный метод, читающий файл и возвращающий словарь созвездий.
         Вызывается при инициализации.
         -----------------------------------------------------------------------
         file_name - имя файла, откуда производится чтение.
-        '''
+        """
         with codecs.open(file_name, encoding='utf-8') as file:
             full_file_str = file.read()
             file.close()
-        stell_dict = {full_file_str[:3] : Node(full_file_str[:3], full_file_str)}
+        stell_dict = {full_file_str[:3]: Node(full_file_str[:3], full_file_str)}
         stell_regex = self._get_regex('\\n\w\w\w')
         stell_str_list = stell_regex.findall(full_file_str)
         for i in range(len(stell_str_list)):
             stell_str_list[i] = stell_str_list[i][1:]
-            stell_dict.update({stell_str_list[i] : Node(stell_str_list[i],
+            stell_dict.update({stell_str_list[i]: Node(stell_str_list[i],
                                full_file_str)})
         return stell_dict
-        
-        
-    def _get_regex(self, template:str):
-        '''
+
+    def _get_regex(self, template: str):
+        """
         Приватный метод, возвращает регулярное выражение по строке.
         -----------------------------------------------------------------------
         template - строка-шаблон для создания регулярного выражения.
-        '''
+        """
         return re.compile(template)
+
 
 '''
 G = Graph('Data.txt')
@@ -144,4 +144,3 @@ print(G.constellations['Crv'].mark)
 G.constellations['Crv'].mark = 1
 print(G.constellations['Crv'].mark)       
 '''
-        
