3c54523677cd19f8aa268f77833eed7cf91b16f4
UxOri <reva.sk@phystech.edu>
2020-12-17 01:33:02 +0300

    Изменён метод is_neighbours, добавлен комментарий

--- model.py
+++ model.py
@@ -2,7 +2,7 @@
 ЗАМЕТКИ
 ------------------
 AttributeError - сделать exception
-
+is_neighbours - реализовать через exception в мейне, так сильно быстрее
 
 
 '''
@@ -15,7 +15,7 @@ import codecs
        
 
 class Node:
-    """
+    '''
     Класс, реализующий вершину двуцветного графа созвездий и ставящий 
     каждому созвездию в соответсвие его имена на русском и латинском а также
     его соседей.
@@ -24,7 +24,7 @@ class Node:
     эта вершина;
     _full_file_str - внутренняя переменная, получаемая при вызове из
     класса Graph, которая содержит информацию о звездном небе в виде строки.
-    """
+    '''
     def __init__(self, stell_name, _full_file_str):
         self.first_name = stell_name
         self.mark = 0
@@ -32,7 +32,7 @@ class Node:
             self._read_neighbours(_full_file_str, self.first_name, )
     
     def _read_neighbours(self, full_file_str, name):
-        """
+        '''
         Приватный метод, находящий для данного созвездия всех его соседей и
         все его имена с помощью поиска в строке full_file_str. Вызывается
         при инициализации.
@@ -40,7 +40,7 @@ class Node:
         name - трёхбуквенное имя созвездия, для которого производится поиск;
         full_file_str - переменная, которая содержит информацию о
         звездном небе в виде строки.
-        """
+        '''
         find_regex = self._get_regex(name + '(-\w+)+:(-\w+)+\.')
         namesList_regex = self._get_regex(name + '(-\w+)+' + '\:')
         neighboursList_regex = self._get_regex('\:' + '(-\w+)+' + '\.')
@@ -79,43 +79,43 @@ class Node:
         return names, neighbours
 
     def _get_regex(self, template: str):
-        """
+        '''
         Приватный метод, возвращает регулярное выражение по строке.
         -----------------------------------------------------------------------
         template - строка-шаблон для создания регулярного выражения.
-        """
+        '''
         return re.compile(template)
         
     def _find_str(self, regex, object_str):
-        """
+        '''
         Приватный метод, возвращает один, первый, результат поиска в строке
         по регулярному выражению.
         -----------------------------------------------------------------------
         regex - регулярное выражение, по которому происходит поиск;
         object_str - строка, в которой происходит поиск.
-        """
+        '''
         _match = regex.search(object_str)
         return _match.group()
     
 
 class Graph:
-    """
+    '''
     Класс, реализующий двуцветный граф созвездий в виде словаря "трехбуквенное
     обозначение созвездия : объект Node этого созвездия".
     ---------------------------------------------------------------------------
     file_name - имя файла, в котором содержится информация о звёздном небе.
-    """
+    '''
     def __init__(self, file_name: str):
         self.file_name = file_name
         self.constellations = self._read_constellations(file_name)
 
     def _read_constellations(self, file_name):
-        """
+        '''
         Приватный метод, читающий файл и возвращающий словарь созвездий.
         Вызывается при инициализации.
         -----------------------------------------------------------------------
         file_name - имя файла, откуда производится чтение.
-        """
+        '''
         with codecs.open(file_name, encoding='utf-8') as file:
             full_file_str = file.read()
             file.close()
@@ -129,25 +129,23 @@ class Graph:
         return stell_dict
 
     def _get_regex(self, template: str):
-        """
+        '''
         Приватный метод, возвращает регулярное выражение по строке.
         -----------------------------------------------------------------------
         template - строка-шаблон для создания регулярного выражения.
-        """
+        '''
         return re.compile(template)
 
     def is_neighbours (self, stell:Node, step_name:str):
+        '''
+        Публичный метод, позволяющий определить, граничит ли указанное
+        созвездие с текущим, возвращает объект Node, отвечающий за проверяемое
+        созвездие, если они граничат и 0, если нет.
+        -----------------------------------------------------------------------
+        stell - объект Node, соответствующий текущему созвездию; 
+        step_name - имя созвездия для проверки.
+        '''
         for i in stell.neighbours:
             if step_name in self.constellations[i].names:
-                return True
+                return self.constellations[i]
         return False
-
-
-G = Graph('Data.txt')
-print(G.constellations['And'].names)
-print(G.constellations['And'].mark)
-G.constellations['And'].mark = 1
-print(G.constellations['And'].mark)     
-A = G.constellations['And']
-print(G.is_neighbours(A, 'Треугольник'))
-
