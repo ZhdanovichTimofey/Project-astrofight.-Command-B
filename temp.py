2f63d0321b42a90137b81501c84ad7f050167580
UxOri <reva.sk@phystech.edu>
2020-12-18 01:58:01 +0300

    Знание того, что мышь однажды сможет сесть за проект не в последнюю ночь... наполняет вас решимостью.

--- temp.py
+++ temp.py
@@ -3,16 +3,16 @@ import numpy as np
 import pygame
 import sys
 import tkinter as t
-import random
 
-def rnd_start_stop (graph:model.Graph):
-    a = 0
+
+
 
 pygame.init()
 
 FPS = 60
 
 stell_graph = model.Graph('Data.txt')
+start_3str, stop_3str = stell_graph.rnd_start_stop()
 
 window = pygame.display.set_mode((800, 670))
 screen = pygame.Surface((800, 670))
@@ -23,13 +23,26 @@ screen = pygame.image.load('master.jpg')
 info = pygame.image.load('name.png')
 window.blit(screen, (0, 150))
 window.blit(info, (70, 0))
+
+f = pygame.font.Font(None, 48)
+start_text = f.render(start_3str, True, (251, 243, 0))
+window.blit(start_text, (150, 186))
+stop_text = f.render(stop_3str, True, (251, 243, 0))
+window.blit(stop_text, (94, 237))
+
 pygame.display.flip()
 
 pygame.display.update()
 clock = pygame.time.Clock()
 finished = False
 
+path1 = []
+path2 = []
+
+
 while not finished:
+    
+    
     clock.tick(FPS)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
